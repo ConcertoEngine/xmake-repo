b9f53db5c0786bec9f74e9e6b55fef756feea8fa
Arthur Vasseur <arthur.vasseur@epitech.eu>
2023-02-20 18:25:59 +0900

    feat: add on_install rule

--- packages/c/concertocore/xmake.lua
+++ packages/c/concertocore/xmake.lua
@@ -5,3 +5,7 @@ package('ConcertoCore')
     set_kind('library')
     add_urls('https://github.com/ConcertoEngine/ConcertoCore.git')
     add_versions('2023.02.20', 'a2fbe5f7e411eff90f68661aaf0a4e87a3fd5386')
+
+    on_install(function (package)
+        import("package.tools.xmake").install(package)
+    end)

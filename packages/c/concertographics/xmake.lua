ea8863f77c1fded71b8e10d17caac7e1d9ef0fde
Arthur Vasseur <arthur.vasseur@epitech.eu>
2023-03-13 14:40:33 +0900

    feat(ConcertoGraphics): new version

--- packages/c/concertographics/xmake.lua
+++ packages/c/concertographics/xmake.lua
@@ -1,10 +1,10 @@
 package('ConcertoGraphics')
     set_homepage('https://github.com/ConcertoEngine/ConcertoGraphics')
-    set_description('Concerto engine core library')
+    set_description('Concerto engine graphics library')
     set_license('MIT')
     set_kind('library')
     add_urls('https://github.com/ConcertoEngine/ConcertoGraphics.git')
-    add_versions('2023.03.13+1', '964a93e9029fabd89e42259d1a2c5d07ad8beea5')
+    add_versions('2023.03.13+2', 'ab589b2644d6db87e81abd804f10193b5ad64e29')
     add_deps('ConcertoCore', 'vulkan-loader', 'vulkan-memory-allocator', 'vk-bootstrap', 'glm', 'stb', 'glfw', 'vulkan-validationlayers', 'imgui')
 
     on_install(function (package)

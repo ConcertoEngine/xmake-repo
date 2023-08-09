13f1392a7603055180465a421e66af4e9359dc4f
Arthur Vasseur <arthur.vasseur@epitech.eu>
2023-08-09 21:40:34 +0200

    fix(ConcertoGraphics): unused deps

--- packages/c/concertographics/xmake.lua
+++ packages/c/concertographics/xmake.lua
@@ -5,7 +5,7 @@ package('ConcertoGraphics')
     set_kind('library')
     add_urls('https://github.com/ConcertoEngine/ConcertoGraphics.git')
     add_versions('2023.08.09', '4caf73756748689c9b7adfcdf94ba74bc6ea1a32')
-    add_deps('ConcertoCore', 'vulkan-loader', 'vulkan-memory-allocator', 'vk-bootstrap', 'glm', 'stb', 'glfw', 'imgui')
+    add_deps('ConcertoCore', 'vulkan-loader', 'vulkan-memory-allocator', 'glm', 'stb', 'glfw', 'imgui')
 
     on_install(function (package)
         import("package.tools.xmake").install(package)

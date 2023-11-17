651b88f1511b23422d99af448e915b0f9ae0ad02
Arthur Vasseur <arthur.vasseur@epitech.eu>
2023-11-17 22:02:39 +0100

    feat(ConcertoEngine): new version

--- packages/c/concertoengine/xmake.lua
+++ packages/c/concertoengine/xmake.lua
@@ -5,8 +5,18 @@ package('ConcertoEngine')
     set_kind('library')
     add_urls('https://github.com/ConcertoEngine/Concerto.git')
     add_versions('2023.16.11+5', '2b5c29b94f366e5666837ba2dfe3fe978e108d59')
-    add_repositories("nazara-repo https://github.com/NazaraEngine/xmake-repo.git")
-    add_deps('ConcertoCore', 'nazaraengine')
+    add_deps('ConcertoCore')
+    add_deps("nazaraengine", {debug=is_mode('debug'), configs={ utility = true,
+                                                                audio = false,
+                                                                renderer = true,
+                                                                joltphysics3d = false,
+                                                                bulletphysics3d = false,
+                                                                graphics = true,
+                                                                plugin_assimp = false,
+                                                                platform = true,
+                                                                chipmunkphysics2d = false,
+                                                                widgets = false,
+                                                                network = false}})
 
     on_install(function (package)
         import("package.tools.xmake").install(package)

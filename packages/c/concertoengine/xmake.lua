8ac5f97494aed7a2a1cacea13c46f50907c3d050
Arthur Vasseur <arthur.vasseur@epitech.eu>
2023-11-18 18:33:53 +0100

    feat(ConcertoEngine): new version

--- packages/c/concertoengine/xmake.lua
+++ packages/c/concertoengine/xmake.lua
@@ -4,19 +4,23 @@ package('ConcertoEngine')
     set_license('MIT')
     set_kind('library')
     add_urls('https://github.com/ConcertoEngine/Concerto.git')
-    add_versions('2023.11.18', '63ba81a103435e977ce251c45eb398f2b9d8c760')
+    add_versions('2023.11.18+1', '63ba81a103435e977ce251c45eb398f2b9d8c760')
     add_deps('ConcertoCore')
-    add_deps("nazaraengine", {debug=is_mode('debug'), configs={ utility = true,
-                                                                audio = false,
-                                                                renderer = true,
-                                                                joltphysics3d = false,
-                                                                bulletphysics3d = false,
-                                                                graphics = true,
-                                                                plugin_assimp = false,
-                                                                platform = true,
-                                                                chipmunkphysics2d = false,
-                                                                widgets = false,
-                                                                network = false}})
+
+    on_load(function (package)
+        package::add_deps('nazraengine', {configs={ debug = ismode('debug')
+                                                    utility = true,
+                                                    audio = false,
+                                                    renderer = true,
+                                                    joltphysics3d = false,
+                                                    bulletphysics3d = false,
+                                                    graphics = true,
+                                                    plugin_assimp = false,
+                                                    platform = true,
+                                                    chipmunkphysics2d = false,
+                                                    widgets = false,
+                                                    network = false}})
+    end)
 
     on_install(function (package)
         import("package.tools.xmake").install(package)

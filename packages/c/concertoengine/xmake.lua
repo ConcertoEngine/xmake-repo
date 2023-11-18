a8bc83d2afec407c732828949da0c00a4465cb72
Arthur Vasseur <arthur.vasseur@epitech.eu>
2023-11-18 18:41:26 +0100

    feat(ConcertoEngine): new version

--- packages/c/concertoengine/xmake.lua
+++ packages/c/concertoengine/xmake.lua
@@ -4,11 +4,11 @@ package('ConcertoEngine')
     set_license('MIT')
     set_kind('library')
     add_urls('https://github.com/ConcertoEngine/Concerto.git')
-    add_versions('2023.11.18+4', '63ba81a103435e977ce251c45eb398f2b9d8c760')
+    add_versions('2023.11.18+5', '63ba81a103435e977ce251c45eb398f2b9d8c760')
     add_deps('ConcertoCore')
 
     on_load(function (package)
-        package:add('deps', 'nazraengine', {configs={ debug = is_mode('debug'),
+        package:add('deps', 'nazraengine', {configs={ debug = package:is_debug(),
                                                     utility = true,
                                                     audio = false,
                                                     renderer = true,

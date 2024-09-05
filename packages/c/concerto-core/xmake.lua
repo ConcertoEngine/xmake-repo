422b1b80ee7179be3bbb12bdd1a74c3035cda81e
Arthur Vasseur <arthur.vasseur@epitech.eu>
2024-09-05 22:15:06 +0200

    feat(ConcertoCore): new version

--- packages/c/concerto-core/xmake.lua
+++ packages/c/concerto-core/xmake.lua
@@ -4,9 +4,12 @@ package('concerto-core')
     set_license('MIT')
     set_kind('library')
     add_urls('https://github.com/ConcertoEngine/ConcertoCore.git')
-    add_versions('2024.08.08', '42e134d3b0c18ee6b826bd960d5d71056feacf07')
+    add_versions('2024.09.05', '925c45c0b0395e27dddb20f7601b74d35956ceda')
     add_deps("nlohmann_json")
 
     on_install(function (package)
+        if package:has_tool("cxx", "cl", "clang_cl") then
+            package:add("cxxflags", "/Zc:preprocessor")
+        end
         import("package.tools.xmake").install(package)
     end)

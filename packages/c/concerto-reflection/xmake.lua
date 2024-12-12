292b51a1cde68d2d470502935efc5fd2469ada6b
Arthur Vasseur <arthur.vasseur@epitech.eu>
2024-12-12 17:59:29 +0100

    fix(ConcertoReflection): remove std_lib configuration from installation process

--- packages/c/concerto-reflection/xmake.lua
+++ packages/c/concerto-reflection/xmake.lua
@@ -8,11 +8,6 @@ package("concerto-reflection")
     add_versions("2024.12.12+5", "0b88f354c18b6fdc3a8c138839f69abdb50503a3")
     add_deps("concerto-core", "pugixml", "eventpp", "catch2")
 
-    add_configs("std_lib", { description = "Enable reflection core library", default = true, type = "boolean" })
-
     on_install(function (package)
-        local configs = {
-            std_lib = package:config("std_lib")
-        }
         import("package.tools.xmake").install(package, configs)
     end)

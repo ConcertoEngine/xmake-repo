4d10d703f672f3ff23a05e8c831a7f3942429a1a
Arthur Vasseur <arthur.vasseur@epitech.eu>
2024-12-12 17:52:30 +0100

    fix(ConcertoReflection): update version to 2024.12.12+5

--- packages/c/concerto-reflection/xmake.lua
+++ packages/c/concerto-reflection/xmake.lua
@@ -5,7 +5,7 @@ package("concerto-reflection")
     set_kind("library")
     add_urls("git@github.com:ConcertoEngine/ConcertoReflection.git")
 
-    add_versions("2024.12.12+4", "b0178fe994b09c4c0596ef66a43b590a0763148c")
+    add_versions("2024.12.12+5", "0b88f354c18b6fdc3a8c138839f69abdb50503a3")
     add_deps("concerto-core", "pugixml", "eventpp", "catch2")
 
     add_configs("std_lib", { description = "Enable reflection core library", default = true, type = "boolean" })

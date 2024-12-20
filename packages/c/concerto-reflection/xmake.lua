b8d744d1a9b7d331e6ed48b0abaf8b7eb0461a23
Arthur <72025980+ArthurVasseur@users.noreply.github.com>
2024-12-20 16:25:56 +0100

    feat(concerto-reflection): new version

--- packages/c/concerto-reflection/xmake.lua
+++ packages/c/concerto-reflection/xmake.lua
@@ -5,7 +5,7 @@ package("concerto-reflection")
     set_kind("library")
     add_urls("git@github.com:ConcertoEngine/ConcertoReflection.git")
 
-    add_versions("2024.12.17", "8249ff967534ec9c023402426ed433405e8deb2a")
+    add_versions("2024.12.20", "b24422a44799dc56df72cf7ca6f01e1e27c63fbd")
     add_deps("concerto-core", "pugixml", "eventpp", "catch2")
 
     on_install(function (package)

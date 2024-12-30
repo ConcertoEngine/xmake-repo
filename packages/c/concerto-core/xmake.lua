b94186bf9345180d07c417c9fd205abf10c3ac62
Arthur <72025980+ArthurVasseur@users.noreply.github.com>
2024-12-30 11:17:35 +0100

    fix(ConcertoCore): deps

--- packages/c/concerto-core/xmake.lua
+++ packages/c/concerto-core/xmake.lua
@@ -5,7 +5,7 @@ package('concerto-core')
     set_kind('library')
     add_urls('https://github.com/ConcertoEngine/ConcertoCore.git')
     add_versions('2024.12.22', '2de7f0ce166ded08a846c20237f2cc6da06f83fe')
-    add_deps("nlohmann_json")
+    add_deps("enet")
 
     on_install(function (package)
         if package:has_tool("cxx", "cl", "clang_cl") then

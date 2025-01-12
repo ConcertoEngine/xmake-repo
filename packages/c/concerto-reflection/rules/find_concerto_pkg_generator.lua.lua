8cc49bf767ceaccea47c3ec586b648021b11f5fc
Arthur Vasseur <arthur.vasseur@hotmail.fr>
2025-01-12 15:18:21 +0100

    fix(concerto-reflection): update package version to 2025.01.12 and enhance environment variable handling for Windows

--- packages/c/concerto-reflection/rules/find_concerto_pkg_generator.lua.lua
+++ packages/c/concerto-reflection/rules/find_concerto_pkg_generator.lua.lua
@@ -10,18 +10,25 @@ rule("find_cct_pkg_generator")
             dir = path.join(cctPkgGen:installdir(), "bin")
         else
             cctPkgGen = project.target("concerto-pkg-generator")
-            if cctPkgGen then
-                dir = cctPkgGen:targetdir()
-                envs = cctPkgGen:get("runenvs")
-				if not envs and os.host() == "linux" or os.host() == "macosx" then
-					envs = {}
-					for _, pkg in ipairs(cctPkgGen:orderpkgs()) do
-						local installDir = path.join(pkg:installdir(), "lib")
-						envs.LD_LIBRARY_PATH = installDir .. path.envsep() .. (envs.LD_LIBRARY_PATH or "")
-					end
-				end
+        end
+
+        if cctPkgGen then
+            dir = cctPkgGen:targetdir()
+            envs = cctPkgGen:get("runenvs")
+            if not envs then
+                envs = {}
+                for _, pkg in ipairs(cctPkgGen:orderpkgs()) do
+                    if os.host() == "linux" or os.host() == "macosx" then
+                        local installDir = path.join(pkg:installdir(), "lib")
+                        envs.LD_LIBRARY_PATH = installDir .. path.envsep() .. (envs.LD_LIBRARY_PATH or "")
+                    elseif os.host() == "windows" then
+                        local installDir = path.join(pkg:installdir(), "bin")
+                        envs.PATH = installDir .. path.envsep() .. (envs.PATH or "")
+                    end
+                end
             end
         end
+        print(envs)
         local program = find_tool("concerto-pkg-generator", {version = false, paths = dir, envs = envs})
         target:data_set("concerto-pkg-generator", program)
         target:data_set("concerto-pkg-generator-envs", envs)
--- packages/c/concerto-reflection/xmake.lua
+++ packages/c/concerto-reflection/xmake.lua
@@ -5,7 +5,7 @@ package("concerto-reflection")
     set_kind("library")
     add_urls("git@github.com:ConcertoEngine/ConcertoReflection.git")
 
-    add_versions("2024.12.22", "286a89af81f86859f094b53aef4068846cdf8734")
+    add_versions("2025.01.12", "ea213b5b7422710cb5fcd2afda0ff5e8613f46d1")
     add_deps("concerto-core", "pugixml", "eventpp", "catch2")
 
     on_install(function (package)

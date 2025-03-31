9742645bcbfff25a189efa62a82d8c01e9042c1d
Arthur Vasseur <arthur.vasseur@hotmail.fr>
2025-03-31 21:06:49 +0200

    fix(concerto-core): update version to 2025.03.31 and add static library definition

--- packages/c/concerto-core/xmake.lua
+++ packages/c/concerto-core/xmake.lua
@@ -4,12 +4,16 @@ package('concerto-core')
     set_license('MIT')
     set_kind('library')
     add_urls('https://github.com/ConcertoEngine/ConcertoCore.git')
-    add_versions('2024.12.22', '2de7f0ce166ded08a846c20237f2cc6da06f83fe')
+    add_versions('2025.03.31', 'b31cf82163f39cf477704786ad90b585bc3dd459')
     add_deps("enet")
+
 
     on_install(function (package)
         if package:has_tool("cxx", "cl", "clang_cl") then
             package:add("cxxflags", "/Zc:preprocessor")
+        end
+        if not package:config("shared") then
+            package:add("defines", "CCT_LIB_STATIC")
         end
         import("package.tools.xmake").install(package)
     end)

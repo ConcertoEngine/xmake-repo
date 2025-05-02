fe90603786c163f8adffb63caa4815691db349ac
Arthur Vasseur <arthur.vasseur@hotmail.fr>
2025-05-02 12:32:56 +0200

    fix(concerto-graphics): update dependencies and version to 2025.05.2

--- packages/c/concerto-graphics/xmake.lua
+++ packages/c/concerto-graphics/xmake.lua
@@ -1,12 +1,29 @@
-package('concerto-graphics')
-    set_homepage('https://github.com/ConcertoEngine/ConcertoGraphics')
-    set_description('Concerto engine graphics library')
-    set_license('MIT')
-    set_kind('library')
-    add_urls('https://github.com/ConcertoEngine/ConcertoGraphics.git')
-    add_versions('2024.03.11+2', 'f36093294c46ef91f47d7e79474e690eefa91b5a')
-    add_deps('ConcertoCore', 'vulkan-loader', 'vulkan-memory-allocator', 'stb', 'glfw', 'imgui')
+package("concerto-graphics")
+    set_homepage("https://github.com/ConcertoEngine/ConcertoGraphics")
+    set_description("Concerto engine graphics library")
+    set_license("MIT")
+    set_kind("library")
+    add_urls("https://github.com/ConcertoEngine/ConcertoGraphics.git")
+    
+    add_versions("2025.05.2", "c3f58d7464c863409585d3e0c74004c9b6d42e51")
+
+    add_configs("shared", {description = "Build shared library.", default = true, type = "boolean", readonly = true})
+    
+    add_deps("volk", "vulkan-headers", "vulkan-memory-allocator", "stb", "libsdl2")
+    add_deps("concerto-core", {configs = {shared = true}})
+    add_deps("imgui", {configs = {sdl2 = true, vulkan = true, with_symbols = true, debug = true}})
 
     on_install(function (package)
         import("package.tools.xmake").install(package)
+    end)
+
+    on_test(function (package)
+        assert(package:check_cxxsnippets({test = [[
+            #include <Concerto/Graphics/RHI/APIImpl.hpp>
+            #include <Concerto/Graphics/RHI/Instance.hpp>
+
+            void test() {
+                cct::gfx::rhi::Instance rInstance;
+            }
+        ]]}, {configs = {languages = "c++20"}}))
     end)

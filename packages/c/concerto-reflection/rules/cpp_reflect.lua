c63914ffa908e299d89ec0233d9f816547dafef2
Arthur Vasseur <arthur.vasseur@hotmail.fr>
2025-07-05 09:00:16 +0200

    feat(concerto-reflection): add cpp_reflect rule and remove xml_reflect rule

---
+++ packages/c/concerto-reflection/rules/cpp_reflect.lua
@@ -0,0 +1,33 @@
+rule("cpp_reflect")
+    add_deps("find_cct_pkg_generator")
+    set_extensions(".hpp")
+
+    on_config(function (target)
+        local targetName = target:name():gsub("-(%a)", function(c) return c:upper() end):gsub("^%a", string.upper)
+        local generatedCpp = path.join(target:autogendir(), targetName .. "Package.gen.cpp")
+
+        target:add("headerfiles", path.join(target:autogendir(), "(" .. targetName .. "Package.gen.hpp)"))
+        target:add("files", generatedCpp, {always_added = true})
+        target:add("includedirs", target:autogendir(), {public = true})
+        target:add("defines", path.basename(targetName):upper() .. "_BUILD")
+    end)
+
+    before_buildcmd_files(function (target, batchcmds, headers, opt)
+        local cctPkgGen = target:data("concerto-pkg-generator")
+        assert(cctPkgGen, "concerto-pkg-generator not found!")
+        local envs = target:data("concerto-pkg-generator-envs")
+        local outputCppFile = path.join(target:autogendir(), target:name() .. "gen.cpp")
+
+        batchcmds:show_progress(opt.progress, "${color.build.object}compiling.reflection")
+        local targetName = target:name():gsub("-(%a)", function(c) return c:upper() end):gsub("^%a", string.upper)
+        local args = { path.join(target:autogendir(), targetName) }
+        for _, header in ipairs(headers.sourcefiles) do
+            table.insert(args, header)
+        end
+    
+        batchcmds:vrunv(cctPkgGen.program, args, {envs = envs})
+        batchcmds:add_depfiles(xmlFile)
+        --batchcmds:add_depvalues() todo add version from cabal
+        batchcmds:set_depmtime(os.mtime(outputCppFile))
+        batchcmds:set_depcache(target:dependfile(outputCppFile))
+    end)
--- packages/c/concerto-reflection/rules/find_concerto_pkg_generator.lua.lua
+++ packages/c/concerto-reflection/rules/find_concerto_pkg_generator.lua.lua
@@ -5,41 +5,19 @@ rule("find_cct_pkg_generator")
 
         local cctPkgGen = project.required_package("concerto-reflection")
         local dir
-		local envs
         if cctPkgGen then
             dir = path.join(cctPkgGen:installdir(), "bin")
-            envs = {}
-
-            for _, lib_file in ipairs(cctPkgGen:get("libfiles")) do
-                local installDir = path.absolute(path.new(lib_file):directory():directory())
-                if os.host() == "linux" or os.host() == "macosx" then
-                    envs.LD_LIBRARY_PATH = path.join(installDir, "lib") .. path.envsep() .. (envs.LD_LIBRARY_PATH or "")
-                elseif os.host() == "windows" then
-                    envs.PATH = path.join(installDir, "bin") .. path.envsep() .. (envs.PATH or "")
-                end
-            end
         else
             cctPkgGen = project.target("concerto-pkg-generator")
             if cctPkgGen then
                 dir = cctPkgGen:targetdir()
-                envs = {}
-                for _, pkg in ipairs(cctPkgGen:orderpkgs()) do
-                    if os.host() == "linux" or os.host() == "macosx" then
-                        local installDir = path.join(pkg:installdir(), "lib")
-                        envs.LD_LIBRARY_PATH = installDir .. path.envsep() .. (envs.LD_LIBRARY_PATH or "")
-                    elseif os.host() == "windows" then
-                        local installDir = path.join(pkg:installdir(), "bin")
-                        envs.PATH = installDir .. path.envsep() .. (envs.PATH or "")
-                    end
-                end
+                print("Using concerto-pkg-generator from target directory: " .. dir)
             else
                 raise("Concerto Reflection package not found")
             end
-
         end
 
-        local program = find_tool("concerto-pkg-generator", {version = false, paths = dir, envs = envs})
+        local program = find_tool("concerto-pkg-generator", {version = false, paths = dir})
 
         target:data_set("concerto-pkg-generator", program)
-        target:data_set("concerto-pkg-generator-envs", envs)
     end)
